808531100efef3f33a06dad494564c4db36117bb
y.yawlad <y.yawlad@gmail.com>
2023-11-23 18:14:39 +0300

    refactor

--- frontend/services/ItemService.ts
+++ frontend/services/ItemService.ts
@@ -2,6 +2,7 @@ import Item from "@/types/Item";
 import { instance } from "./service.api.config";
 import Tag from "@/types/Tag";
 import handleAxiosError from "@/utils/handleAxiosError";
+import Like from "@/types/Like";
 
 const ItemService = {
   getItems(params?: { search?: string; tag?: string }) {
@@ -14,9 +15,25 @@ const ItemService = {
   },
   getItem(id: number) {
     return instance
-      .get(`/item/${id}/`)
+      .get(`/items/${id}/`)
       .then((response) => {
         return response.data as Item;
+      })
+      .catch(handleAxiosError);
+  },
+  postItem(data) {
+    return instance
+      .delete(`/items/`)
+      .then((response) => {
+        return true;
+      })
+      .catch(handleAxiosError);
+  },
+  patchItem(id: number) {
+    return instance
+      .delete(`/items/${id}`)
+      .then((response) => {
+        return true;
       })
       .catch(handleAxiosError);
   },
@@ -25,6 +42,30 @@ const ItemService = {
       .get("/items/tags/")
       .then((response) => {
         return response.data as Tag[];
+      })
+      .catch(handleAxiosError);
+  },
+  deleteItem(id: number) {
+    return instance
+      .delete(`/items/${id}`)
+      .then((response) => {
+        return true;
+      })
+      .catch(handleAxiosError);
+  },
+  deleteLike(id: number) {
+    return instance
+      .delete(`/items/likes/${id}`)
+      .then((response) => {
+        return true;
+      })
+      .catch(handleAxiosError);
+  },
+  postLike(data: { item: number }) {
+    return instance
+      .post(`/items/likes/`, data)
+      .then((response) => {
+        return response.data as Like;
       })
       .catch(handleAxiosError);
   },
