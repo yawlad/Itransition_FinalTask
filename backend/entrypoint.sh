ce215e45248e5cf82e4a8d37dcaa03dfa16e7c52
y.yawlad <y.yawlad@gmail.com>
2023-11-24 10:13:31 +0300

    updated entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,6 +11,7 @@ echo "PostgreSQL database is up and running."
 
 echo "Applying database migrations..."
 python manage.py migrate
+python manage.py migrate --run-syncdb
 
 echo "Starting Django server..."
 python manage.py runserver 0.0.0.0:8000
