8b5728cf51c6180295298af1de6674eae1f31b96
y.yawlad <y.yawlad@gmail.com>
2023-11-24 10:38:07 +0300

    updated env

--- frontend/services/service.api.config.ts
+++ frontend/services/service.api.config.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const instance = axios.create({
   withCredentials: true,
-  baseURL: process.env.BACKEND_URL || `http://127.0.0.1:8000/api/v1/`,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || `http://127.0.0.1:8000/api/v1/`,
 });
